a93fb50ccfc78d023924c9c4c5329e0754857610
Bartek Pierściński <bartoszjpierscinski@gmail.com>
2021-03-10 15:40:29 +0100

    making the left rectangle content change on click

---
+++ WEBSITE/script.js
@@ -0,0 +1,11 @@
+
+var about = document.getElementById("aboutrect");
+
+
+function opacityChange() {
+  about.classList.add("opacityfull");
+}
+
+function opacityNone() {
+  about.classList.remove("opacityfull");
+}
