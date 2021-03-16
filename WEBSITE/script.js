e3525f1b54f37f8e062d22a51311f4bbcccc1cf9
blobsaland <68444206+blobsaland@users.noreply.github.com>
2021-03-16 19:44:40 +0100

    big changes
    added podcast covers
    started on javascript for podcast player

--- WEBSITE/script.js
+++ WEBSITE/script.js
@@ -1,11 +1,43 @@
+const playpause = document.querySelector(".playpause");
 
-var about = document.getElementById("aboutrect");
+let audio = new Audio("./pocasts/leon/podcast_1.mp3");
 
+let playtime = document.getElementById("playtime");
 
-function opacityChange() {
-  about.classList.add("opacityfull");
+playPause.onclick = function() {
+if (audio.paused) {
+    audio.play();
+    // playpause.classList.remove("icon-play")
+    // playpause.classList.add("icon-stop")
+}
+else {
+    audio.pause();
+    // playpause.classList.remove("icon-stop")
+    // playpause.classList.add("icon-play")
+  };
+};
+
+const slowclock = function() {
+  playtime.value = audio.currentTime / audio.duration * 100;
+  // timeindicator1.innerHTML = Math.round(audio.currentTime / 60) + ":" + Math.round(audio.currentTime % 60);
+  // time2.value = george.currentTime / george.duration * 100;
+  // timeindicator2.innerHTML = Math.round(george.currentTime / 60) + ":" + Math.round(george.currentTime % 60);
 }
 
-function opacityNone() {
-  about.classList.remove("opacityfull");
+setIntervalBar = setInterval(slowclock, 1000);
+
+playtime.onchange = function() {
+  audio.currentTime = audio.duration * (time1.value / 100);
 }
+
+
+// var about = document.getElementById("aboutrect");
+
+
+// function opacityChange() {
+//   about.classList.add("opacityfull");
+// }
+
+// function opacityNone() {
+//   about.classList.remove("opacityfull");
+// }
