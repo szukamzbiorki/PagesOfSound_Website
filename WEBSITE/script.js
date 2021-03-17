56f8140e46a45c52a5b32e9baa18a4b0bcd3d559
blobsaland <68444206+blobsaland@users.noreply.github.com>
2021-03-17 09:55:21 +0100

    podcast selector

--- WEBSITE/script.js
+++ WEBSITE/script.js
@@ -1,14 +1,31 @@
 const playpause = document.querySelector(".playpause");
 let audio = new Audio("./podcasts/leon/podcast_1.mp3");
 let playtime = document.querySelector(".playtime");
+const podcastone = document.querySelector(".one");
+const podcasttwo = document.querySelector(".two");
+const podcasttree = document.querySelector(".tree");
+
+//podcastselector123
+podcastone.addEventListener('click', function(e) {
+  audio.src = "./podcasts/leon/podcast_1.mp3"
+});
+podcasttwo.addEventListener('click', function(e) {
+  audio.src = "./podcasts/leon/podcast_2.mp3"
+});
+podcasttree.addEventListener('click', function(e) {
+  audio.src = "/podcasts/" + name of selected + "/podcast_3.mp3"
+});
+
 
 // playpause button
 playpause.onclick = function() {
 if (audio.paused) {
     audio.play();
+    playpause.innerHTML = "pause"
 }
 else {
     audio.pause();
+    playpause.innerHTML = "play"
   };
 };
 
