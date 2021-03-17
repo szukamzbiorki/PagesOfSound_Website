c85b5a362d89f5d41bf036386bcbca713985b9b9
blobsaland <68444206+blobsaland@users.noreply.github.com>
2021-03-17 09:32:03 +0100

    small change

--- WEBSITE/script.js
+++ WEBSITE/script.js
@@ -24,12 +24,9 @@ playtime.onchange = function() {
 
 
 // var about = document.getElementById("aboutrect");
-
-
 // function opacityChange() {
 //   about.classList.add("opacityfull");
 // }
-
 // function opacityNone() {
 //   about.classList.remove("opacityfull");
 // }
