49fc72abbccb1fafe9a97ebb62254eda22a7a931
blobsaland <68444206+blobsaland@users.noreply.github.com>
2021-03-19 10:41:30 +0100

    hallo

--- WEBSITE/script.js
+++ WEBSITE/script.js
@@ -1,30 +1,40 @@
 const playpause = document.querySelector(".playpause");
-let audio = new Audio("./podcasts/leon/podcast_1.mp3");
+
+let audio = new Audio("./podcasts/Bartosz Pierscinski/podcast_1.mp3");
+
 let playtime = document.querySelector(".playtime");
 let volumeslider = document.querySelector(".volume");
 const player = document.querySelector(".player");
+
 const podcastone = document.querySelector(".one");
 const podcasttwo = document.querySelector(".two");
 const podcasttree = document.querySelector(".tree");
-const namename = document.querySelector(".name");
+
+let Name = document.querySelector(".namewrapper");
+
+const coverimg1 = document.querySelector(".coverimg1")
+const coverimg2 = document.querySelector(".coverimg2")
+const coverimg3 = document.querySelector(".coverimg3")
 
 //name selector
-namename.addEventListener('click', function(e) {
-  console.log(e);
+Name.addEventListener("click", function (clickname) {
+  console.log("./podcasts/"+clickname.target.lastChild.textContent+"/cover_1.jpg");
+  coverimg1.src = "./podcasts/"+clickname.target.lastChild.textContent+"/cover_1.jpg"
+  coverimg2.src = "./podcasts/"+clickname.target.lastChild.textContent+"/cover_2.jpg"
+  coverimg3.src = "./podcasts/"+clickname.target.lastChild.textContent+"/cover_3.jpg"
 });
 
 //podcastselector123
 podcastone.addEventListener('click', function(e) {
-  audio.src = "./podcasts/leon/podcast_1.mp3"
-  console.log(e);
-
+  audio.src = "./podcasts/Bartosz Pierscinski/podcast_1.mp3"
+  console.log(clicknamefunc());
 });
 podcasttwo.addEventListener('click', function(e) {
-  audio.src = "./podcasts/leon/podcast_2.mp3"
+  audio.src = "./podcasts/Bartosz Pierscinski/podcast_2.mp3"
 });
-// podcasttree.addEventListener('click', function(e) {
-//   audio.src = "/podcasts/" + name of selected + "/podcast_3.mp3"
-// });
+podcasttree.addEventListener('click', function(e) {
+  audio.src = "/podcasts/"+ +"/podcast_3.mp3"
+});
 
 // playpause button
 playpause.addEventListener('click', function(e) {
