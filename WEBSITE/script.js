8932354bdf3806dec4412d941c1793ed49a1ef77
Stefaniia Bodnia <stefa.bodnya@icloud.com>
2021-06-01 20:34:02 +0200

    uploads

--- WEBSITE/script.js
+++ WEBSITE/script.js
@@ -1,7 +1,7 @@
 //play pause square
 const playpause = document.querySelector(".playpause");
 const squarediv = document.querySelector(".squarediv");
-
+// var cursor = document.querySelector("#cursor");
 
 var globaldata;
 // squarediv.style.width = squarediv.offsetHeight;
@@ -11,14 +11,68 @@ var globaldata;
 
 //slide up on click
 
-$("#infobar").click(function(){
+$("#infobar").click(function() {
   $("#infobar").toggleClass("up1");
   $("#inforect").toggleClass("up2");
 });
 
+// window.addEventListener("mousemove", function(){
+//
+//   var x = e.pageX;
+//   var y = e.pageY;
+//   cursor.style.left = x + "px";
+//   $(".coursorFollower").css("top", y + "px");
+// });
 
-let current_student_name = "bartosz";
-let audio = new Audio("./podcasts/"+current_student_name+"/podcast_1.mp3");
+let circle = document.getElementById('cursor');
+const onMouseMove = (e) => {
+  circle.style.left = (e.pageX + 40) + 'px';
+  circle.style.top = (e.pageY + 5) + 'px';
+}
+
+const showCursor = (e) => {
+  circle.classList.add("showCursorClass");
+}
+
+const hideCursor = (e) => {
+  circle.classList.remove("showCursorClass");
+}
+
+$('.message-close').on('click', function() {
+  $('.message').hide();
+});
+$('.specialEpisode').hide();
+
+$('.specialClose').on('click', function() {
+  $('.specialEpisode').hide();
+});
+
+$('.special').on('click', function() {
+  $('.specialEpisode').show();
+});
+
+window.onload = function(){
+  document.getElementById("loader").style.opacity = "0";
+  setTimeout(function(){ document.getElementById("loader").style.display = "none"; }, 1000);
+}
+
+var text = ['loading.', 'loading..', 'loading...'];
+var counter = 0;
+var elem = document.getElementById("loader");
+var inst = setInterval(change, 500);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
+
+
+let current_student_name = "leon";
+let audio = new Audio();
 
 let playtime = document.querySelector(".playtime");
 let volumeslider = document.querySelector(".volume");
@@ -28,14 +82,12 @@ const time = document.querySelector(".time");
 const podcastone = document.querySelector("#one");
 const podcasttwo = document.querySelector("#two");
 const podcasttree = document.querySelector("#three");
-const podcastfour = document.querySelector("#four");
 
 let names = document.querySelector(".namewrapper");
 
 const coverimg1 = document.querySelector(".coverimg1");
 const coverimg2 = document.querySelector(".coverimg2");
 const coverimg3 = document.querySelector(".coverimg3");
-const coverimg4 = document.querySelector(".coverimg4");
 
 const leftinfo = document.querySelector("#leftinfo");
 const rightinfo = document.querySelector("#rightinfo");
@@ -43,54 +95,48 @@ const inforect = document.querySelector("#inforect");
 // const contents_indiv = document.querySelector("#contents_indiv");
 
 //name selector
-names.addEventListener("click", function (clickname) {
+names.addEventListener("click", function(clickname) {
   current_student_name = clickname.target.dataset.name;
   setFiles(current_student_name);
 });
+
+document.addEventListener('mousemove', onMouseMove);
+infobar.addEventListener('mouseover', showCursor);
+infobar.addEventListener('mouseleave', hideCursor);
 
 
 //active selector
 var NAME = document.querySelectorAll('.name');
 
 NAME.forEach(function(el, i) {
-  el.addEventListener('mousedown', function(event) {
-  	reset(NAME);
-		event.target.classList.add('active');
+  el.addEventListener('click', function(event) {
+    reset(NAME);
+    event.target.classList.add('active');
   });
 });
 
-function reset(NAME){
-	NAME.forEach(function(el, i) {
-		el.classList.remove('active');
+function reset(NAME) {
+  NAME.forEach(function(el, i) {
+    el.classList.remove('active');
   });
 }
 
 function setFiles(current_student_name) {
-<<<<<<< Updated upstream
-  coverimg1.src = "./podcasts/"+current_student_name+"/cover_1.jpg";
-  coverimg2.src = "./podcasts/"+current_student_name+"/cover_2.jpg";
-  coverimg3.src = "./podcasts/"+current_student_name+"/cover_3.jpg";
-=======
   coverimg1.src = "./podcasts/" + current_student_name + "/cover_1.jpg";
   coverimg2.src = "./podcasts/" + current_student_name + "/cover_2.jpg";
   coverimg3.src = "./podcasts/" + current_student_name + "/cover_3.jpg";
-  coverimg4.src = "./podcasts/" + current_student_name + "/cover_4.jpg";
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
-  $.getJSON("./podcasts/"+current_student_name+"/contents.json", function(json){
+  $.getJSON("./podcasts/" + current_student_name + "/contents.json", function(json) {
     globaldata = json;
-    var titles=[];
+    var titles = [];
     titles[0] = globaldata.title1;
     titles[1] = globaldata.title2;
     titles[2] = globaldata.title3;
     // leftinfo.innerHTML = globaldata.title1;
     inforect.innerHTML = globaldata.description;
-    $(".podcasts").scroll(function (event) {
-        var scroll = $(".podcasts").scrollTop();
-        // console.log(scroll);
+    $(".podcasts").scroll(function(event) {
+      var scroll = $(".podcasts").scrollTop();
+      // console.log(scroll);
 
       // console.log(podcasts);
       let current = "";
@@ -106,8 +152,8 @@ function setFiles(current_student_name) {
           // console.log
         }
       });
-    rightinfo.innerHTML = current + "/3";
-    leftinfo.innerHTML = titles[current-1];
+      rightinfo.innerHTML = current + "/3";
+      leftinfo.innerHTML = titles[current - 1];
 
     });
 
@@ -117,30 +163,23 @@ function setFiles(current_student_name) {
 
 //podcastselector123
 podcastone.addEventListener('click', function() {
-  audio.src = "./podcasts/"+current_student_name+"/podcast_1.mp3";
+  audio.src = "./podcasts/" + current_student_name + "/podcast_1.mp3";
 });
 podcasttwo.addEventListener('click', function() {
-  audio.src = "./podcasts/"+current_student_name+"/podcast_2.mp3";
+  audio.src = "./podcasts/" + current_student_name + "/podcast_2.mp3";
 });
 podcasttree.addEventListener('click', function() {
-  audio.src = "./podcasts/"+current_student_name+"/podcast_3.mp3";
-});
-podcastfour.addEventListener('click', function() {
-  audio.src = "./podcasts/" + current_student_name + "/podcast_4.mp3";
-});
-podcastfour.addEventListener('click', function() {
-  audio.src = "./podcasts/" + current_student_name + "/podcast_4.mp3";
+  audio.src = "./podcasts/" + current_student_name + "/podcast_3.mp3";
 });
 
 setFiles(current_student_name);
 
 // playpause button
 playpause.addEventListener('click', function(e) {
-if (audio.paused) {
+  if (audio.paused) {
     audio.play();
     playpause.innerHTML = "&#x25A0"
-}
-else {
+  } else {
     audio.pause();
     playpause.innerHTML = "&#x25B6"
   };
@@ -149,12 +188,11 @@ else {
 //playtimeslider
 const slowclock = function() {
   playtime.value = audio.currentTime / audio.duration * 600;
-  if((Math.round(audio.currentTime % 60))<10){
-  time.innerHTML = Math.round(audio.currentTime / 60) + ":0" + Math.round(audio.currentTime % 60) + "/" + Math.round(audio.duration / 60) + ":" + Math.round(audio.duration % 60);
+  if ((Math.round(audio.currentTime % 60)) < 10) {
+    time.innerHTML = Math.round(audio.currentTime / 60) + ":0" + Math.round(audio.currentTime % 60) + "/" + Math.round(audio.duration / 60) + ":" + Math.round(audio.duration % 60);
+  } else {
+    time.innerHTML = Math.round(audio.currentTime / 60) + ":" + Math.round(audio.currentTime % 60) + "/" + Math.round(audio.duration / 60) + ":" + Math.round(audio.duration % 60);
   }
-  else{
-  time.innerHTML = Math.round(audio.currentTime / 60) + ":" + Math.round(audio.currentTime % 60) + "/" + Math.round(audio.duration / 60) + ":" + Math.round(audio.duration % 60);
- }
   // timeindicator1.innerHTML = Math.round(audio.currentTime / 60) + ":" + Math.round(audio.currentTime % 60);
 };
 
@@ -169,7 +207,7 @@ playtime.onchange = function() {
 
 // volume slider
 volumeslider.onchange = function() {
-  audio.volume = volumeslider.value/100;
+  audio.volume = volumeslider.value / 100;
 };
 
 
